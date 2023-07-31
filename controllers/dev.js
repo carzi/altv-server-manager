071f1c657607ad6d0abc92e9f3f09d1fc7e9b363
CARZI <70021101+carzi@users.noreply.github.com>
2023-07-31 07:24:23 +0300

    v0.1.1

--- controllers/dev.js
+++ controllers/dev.js
@@ -1,24 +1,21 @@
-const { spawn } = require("child_process");
 const Build = require("../utils/build");
 const chokidar = require("chokidar");
 const { join } = require("path");
+const { execute, path } = require("../utils/execute");
 
 module.exports = async (...args) => {
   await Build(false);
-  const path = join(process.cwd(), "altv-server");
-
-  let proc = spawn(join(path, "altv-server.exe"), { cwd: path, stdio: "inherit" });
-
+  let proc = execute();
   chokidar
     .watch(join(process.cwd(), "modules"), {
       cwd: path,
       ignored: "!*.ts",
     })
-    .on("change", async (file) => {
+    .on("change", async () => {
       proc.kill();
       process.chdir(join(path, ".."));
       await Build(false);
       process.chdir(path);
-      proc = spawn(join(path, "altv-server.exe"), { cwd: path, stdio: "inherit" });
+      proc = execute();
     });
 };
--- controllers/start.js
+++ controllers/start.js
@@ -1,9 +1,7 @@
-const { spawn } = require("child_process");
-const { join } = require("path");
 const Build = require("../utils/build");
+const {execute} = require("../utils/execute");
 
 module.exports = async (...args) => {
   await Build(args[0].m);
-  const path = join(process.cwd(), "altv-server");
-  spawn(join(path, "altv-server.exe"), { cwd: path, stdio: "inherit" });
+  execute();
 };
